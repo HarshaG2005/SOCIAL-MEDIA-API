224892f6c34f353be3450808db8b4d078c7d2a35
Harsha Gayantha <harshaa7654@gmail.com>
2026-02-21 07:27:39 +0530

    Remove cart router implementation

--- app/routers/cart.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-
-import app.models
-import app.oauth2
-from app.database import get_db
-from app.schemas import CartCreate, CartUpdate
-
-router = APIRouter(prefix="/cart", tags=["Cart"])
