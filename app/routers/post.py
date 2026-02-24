59b10193bd8172893bad7db7e3e5add47d2e87f1
Harsha Gayantha <harshaa7654@gmail.com>
2026-02-24 10:48:47 +0530

    Black formatting

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
-from fastapi import (APIRouter, Depends, FastAPI, HTTPException, Query,
-                     Request, status)
+from fastapi import APIRouter, Depends, FastAPI, HTTPException, Query, Request, status
 from sqlalchemy import func
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
